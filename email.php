7478d94835fa54938aabebc14bd6e832bb8eeace
Anderson  Aparecido Stos Gomes <andersongomes@MacBook-Air-de-Anderson.local>
2020-11-19 23:38:50 -0300

    implementacao do email e codigo em PHP

---
+++ email.php
@@ -0,0 +1,35 @@
+<?php 
+if(isset($_POST['email']) && !empty($_POST['email'])){
+
+  $nome = addslashes($_POST['nome']);
+  $email = addslashes($_post['email']);
+  $assunto = addslashes($_POST['assunto']);
+  $mensagem = addslashes($_POST['mensagem']);
+
+
+  $to = "atendimento@cashbackfitness.com.br";
+  $subject = "Fale Conosco - atendimento br";
+  $body = "Nome :".$nome. "\r\n".
+           "Email".$email."\r\n".
+           "Assunto".$assunto."\r\n".
+           "Mensagem".$mensagem;
+           
+
+  $header = "From:atendimento@cashbackfitness.com.br"."\r\n".
+            "Reply-To:".$email."\r\n".
+            "X=Mailer:PHP/".phpversion();
+
+
+   if(mail($to,$subject,$body,$header)){
+         echo("Email enviado com sucesso.ativo");
+   }else{
+     echo("O Email nao pode ser enviado.");
+   }
+
+}
+
+
+
+
+
+?>
